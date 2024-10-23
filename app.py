31da699b909be210c92749ed23dda80f7170f86c
Darshan-BK <darshan.bk111@gmail.com>
2024-10-23 23:29:50 +0000

    Initial commit for Dockerized Flask app

--- app.py
+++ app.py
@@ -56,3 +56,8 @@ def delete_item(id):
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+
+
+@app.route('/')
+def index():
+    return jsonify({'message': 'Welcome to the Items API!'})
