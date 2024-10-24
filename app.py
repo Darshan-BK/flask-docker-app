9039856eac9086191287651f710b4fced5752b82
Darshan-BK <darshan.bk111@gmail.com>
2024-10-24 00:24:48 +0000

    Initial commit: Add Flask app, Dockerfile, and docker-compose.yml

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Configure SQLite database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///items.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instances/items.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
